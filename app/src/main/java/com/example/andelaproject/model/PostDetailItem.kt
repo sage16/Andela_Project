f5b80869d8372770b330aac71fcd669957d551ba
Nicholas Edoseghe <nedoseghe@gmail.com>
2020-10-03 15:42:03 +0100

    add toolbar

--- app/src/main/java/com/example/andelaproject/model/PostDetailItem.kt
+++
@@ -1,8 +0,0 @@
-package com.example.andelaproject.model
-
-class PostDetailItem (
-    val firstName: String?,
-    val lastName: String?,
-    val emailAddress: String?,
-    val githubLink: String?
-)
